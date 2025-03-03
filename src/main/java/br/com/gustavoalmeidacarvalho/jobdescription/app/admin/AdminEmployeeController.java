719ef443bf3b20d6789d032d83d52e1be1abb0fa
gustxvo <gualmeida2004@gmail.com>
2025-03-03 15:22:15 -0300

    feat: implement new admin home page

--- src/main/java/br/com/gustavoalmeidacarvalho/jobdescription/app/admin/AdminEmployeeController.java
+++ src/main/java/br/com/gustavoalmeidacarvalho/jobdescription/app/admin/AdminEmployeeController.java
@@ -2,10 +2,14 @@ package br.com.gustavoalmeidacarvalho.jobdescription.app.admin;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -13,7 +17,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminEmployeeController {
 
     @GetMapping
-    public String listEmployees() {
+    public String listEmployees(Model model) {
+        List<EmployeeDto> employees = List.of(
+                new EmployeeDto("00000001", "Alberto", "alberto@acme.com", "TI", "Team Lead", BigDecimal.valueOf(3500), "Ativo")
+        );
+        model.addAttribute("employees", employees);
         return "admin/home";
     }
 
